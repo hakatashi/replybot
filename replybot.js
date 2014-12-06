df55f6aa9d79cf90c0a3fed89cc965918d9504c1
Koki Takahashi <hakatasiloving@gmail.com>
2014-12-06 23:29:26 +0900

    Enable logging by sqlite

--- replybot.js
+++ replybot.js
@@ -3,6 +3,7 @@ var express = require('express');
 var morgan = require('morgan');
 var socketio = require('socket.io');
 var seedrandom = require('seedrandom');
+var sqlite3 = require('sqlite3');
 
 var names = require('./names.json');
 
@@ -10,16 +11,21 @@ var client = new elasticsearch.Client({
 	host: 'localhost:9200'
 });
 
-// chat process
+var db = new sqlite3.Database('log.sqlite3');
+var io;
 
-var chatlog = [];
+// chat process
 
 function speak(name, text) {
 	io.emit('chat', JSON.stringify({
 		name: name,
 		text: text
 	}));
-	chatlog.push({name: name, text: text});
+	db.run('INSERT INTO log VALUES($date, $name, $text)', {
+		$date: Number(new Date()),
+		$name: name,
+		$text: text
+	});
 }
 
 function ask(name, text) {
@@ -88,45 +94,50 @@ function getName(id) {
 	return names[Math.floor(seedrandom(id)() * names.length)]
 }
 
-// Launch web server
+db.run('CREATE TABLE IF NOT EXISTS log (date INTEGER, name TEXT, text TEXT)', function () {
+	// Launch web server
 
-var app = express();
+	var app = express();
 
-app.use(morgan('combined'));
+	app.use(morgan('combined'));
 
-app.get('/chatlog', function (req, res, next) {
-	res.json(chatlog);
-});
-
-app.use(express.static(__dirname));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	res.sendFile(__dirname + '/404.html');
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: err
+	app.get('/chatlog', function (req, res, next) {
+		db.all('SELECT name, text FROM log ORDER BY date DESC LIMIT 100', function (error, rows) {
+			if (error) return console.error(error);
+			res.json(rows.reverse());
+		});
 	});
-});
 
-var server = app.listen(10724, function () {
-	console.log('Express server listening on port ' + server.address().port);
-});
+	app.use(express.static(__dirname));
 
-// WebSocket Configuration
+	// catch 404 and forward to error handler
+	app.use(function (req, res, next) {
+		var err = new Error('Not Found');
+		err.status = 404;
+		res.sendFile(__dirname + '/404.html');
+	});
 
-var io = socketio(server);
+	// error handler
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 
-io.on('connection', function (socket) {
-	socket.on('chat', function (data) {
-		speak(getName(socket.handshake.address), data);
-		ask(getName(socket.handshake.address), data);
+	var server = app.listen(10724, function () {
+		console.log('Express server listening on port ' + server.address().port);
+	});
+
+	// WebSocket Configuration
+
+	io = socketio(server);
+
+	io.on('connection', function (socket) {
+		socket.on('chat', function (data) {
+			speak(getName(socket.handshake.address), data);
+			ask(getName(socket.handshake.address), data);
+		});
 	});
 });
