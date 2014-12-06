75d64ffeb6dce7b532eaaabdbf9d71116f3ff2e3
Koki Takahashi <hakatasiloving@gmail.com>
2014-12-06 22:12:30 +0900

    Implement communication with server

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+var socket = io();
 
+function refresh() {
+	$.get('/chatlog').done(function (data) {
+		$('#log').empty();
+		data.forEach(function (row) {
+			onChat(row);
+		});
+	});
+	scroll();
+}
+
+function onChat(data) {
+	var $row = $('<tr/>');
+	$row.append($('<th/>', {
+		text: data.name
+	}));
+	$row.append($('<td/>', {
+		text: data.text
+	}));
+	$('#log').append($row);
+}
+
+function scroll() {
+	$('#logspace').animate({
+		scrollTop: $('#log').height()
+	}, 'slow');
+}
+
+$('#input').keypress(function (event) {
+	if (event.which === 13) {
+		socket.emit('chat', $(this).val());
+		$(this).val('');
+		return false;
+	}
+});
+
+socket.on('chat', function (text) {
+	var data = JSON.parse(text);
+	onChat(data);
+	scroll();
+});
+
+$(document).ready(function () {
+	refresh();
+})
--- replybot.js
+++ replybot.js
@@ -7,11 +7,32 @@ var client = new elasticsearch.Client({
 	host: 'localhost:9200'
 });
 
+// chat process
+
+var chatlog = [];
+
+function speak(name, text) {
+	io.emit('chat', JSON.stringify({
+		name: name,
+		text: text
+	}));
+	chatlog.push({name: name, text: text});
+}
+
+function ask(name, text) {
+
+}
+
 // Launch web server
 
 var app = express();
 
 app.use(morgan('combined'));
+
+app.get('/chatlog', function (req, res, next) {
+	res.json(chatlog);
+});
+
 app.use(express.static(__dirname));
 
 // catch 404 and forward to error handler
@@ -39,8 +60,13 @@ var server = app.listen(10724, function () {
 var io = socketio(server);
 
 io.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
+	console.log('connected');
+	socket.on('chat', function (data) {
+		speak('someone', data);
+		ask('soneone', data);
 	});
 });
+
+setInterval(function () {
+	speak('test', new Date().toString());
+}, 3000);
